e9e61f56478dc59046cefbe7d3249de2e18af76c
JC Pamo <josec.pamo@gmail.com>
2022-11-11 19:00:01 -0600

    Adding useFetch

--- src/Components/Pokemons.js
+++ src/Components/Pokemons.js
@@ -1,36 +1,38 @@
-import React, { useState, useEffect } from 'react';
-import Card from './Card';
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useState, useEffect } from "react";
+import { useFetch } from "../Hooks/useFetch";
+import Card from "./Card";
 
 export default function Pokemons(props) {
     const [pokemons, setPokemons] = useState([]);
-
-    useEffect(() => {       
-        const getPokemons = async(url) => {
-            const res = await fetch(url);
-            const json = await res.json();
-
-            json.results.forEach(async (el) => {
-                const res = await fetch(el.url);
-                const data = await res.json();
-
-                const pokemon = {
-                    id: data.id,
-                    name: data.name,
-                    img: data.sprites.front_default
-                }
-
-                setPokemons((pokemons) => [...pokemons, pokemon])
-            });
-        }
-        
-        getPokemons("https://pokeapi.co/api/v2/pokemon");
-    }, []);
-
-    return(
-        <>
-            {(pokemons.lengt===0) 
-            ? (<img className='loader' src='loader.svg' alt='loader'/>) 
-            : (pokemons.map(el => <Card key={el.id} url={el.img} name={el.name}/>))}
-        </>
+    const { data, isPending, error } = useFetch(
+      "https://pokeapi.co/api/v2/pokemon/"
     );
+  
+  useEffect(() => {
+    if (!isPending) {
+      data.results.forEach(async (el) => {
+        const res = await fetch(el.url);
+        const data = await res.json();
+
+        let pokemon = {
+          id: data.id,
+          name: data.name,
+          img: data.sprites.front_default,
+        };
+
+        setPokemons((pokemons) => [...pokemons, pokemon]);
+        });
+    }
+}, [isPending]);
+
+  return (
+    <>
+      {pokemons.length === 0 ? (
+        <img className="loader" src="loader.svg" alt="loader" />
+      ) : (
+        pokemons.map((el) => <Card key={el.id} url={el.img} name={el.name} />)
+      )}
+    </>
+  );
 }
---
+++ src/Hooks/useFetch.js
@@ -0,0 +1,36 @@
+/* eslint-disable no-throw-literal */
+import { useState, useEffect } from 'react';
+
+export const useFetch = url => {
+    const [data, setData] = useState(null);
+    const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null);
+    
+    
+    useEffect(() => {
+        const getData = async url => {
+            try {
+                let res = await fetch(url);
+                
+                if (!res.ok) throw ({
+                    error:true, 
+                    status:res.status, 
+                    statusText: res.statusText || "An error has ocurred"
+                });
+         
+                let data = await res.json();
+    
+                setData(data);
+                setIsPending(false);
+                setError({error:false});
+            } 
+            catch (error) {
+                setIsPending(true);
+                setError(error);
+            }
+        };
+        getData(url);
+    }, [url]);
+    
+    return { data, isPending, error };
+}
