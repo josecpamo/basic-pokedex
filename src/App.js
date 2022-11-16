e8a7049296507073dcccbcdd3bb112f677a21f68
JC Pamo <josec.pamo@gmail.com>
2022-11-16 00:45:10 -0600

    Adding error displaying

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import Header from './Components/Header';
-import Footer from './Components/Footer';
-import Pokemons from './Components/Pokemons';
-import './App.css';
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+import Pokemons from "./Components/Pokemons";
+import "./App.css";
 
 function App() {
-  return (
-    <>
-      <Header></Header>
-      <main className="main">
-        <section className="container">
-          <article className="pokemons">
-            <Pokemons/>
-          </article>
-        </section>
-      </main>
-      <Footer></Footer>
-    </>
-  );
+    return (
+        <>
+            <Header></Header>
+            <main className="main">
+                <section className="container">
+                    <article className="pokemons">
+                        <Pokemons />
+                    </article>
+                </section>
+            </main>
+            <Footer></Footer>
+        </>
+    );
 }
 
 export default App;
--- src/Components/Pokemons.js
+++ src/Components/Pokemons.js
@@ -6,33 +6,42 @@ import Card from "./Card";
 export default function Pokemons(props) {
     const [pokemons, setPokemons] = useState([]);
     const { data, isPending, error } = useFetch(
-      "https://pokeapi.co/api/v2/pokemon/"
+        "https://pokeapi.co/api/v2/pokemon/"
     );
-  
-  useEffect(() => {
-    if (!isPending) {
-      data.results.forEach(async (el) => {
-        const res = await fetch(el.url);
-        const data = await res.json();
 
-        let pokemon = {
-          id: data.id,
-          name: data.name,
-          img: data.sprites.front_default,
-        };
+    useEffect(() => {
+        if (!isPending) {
+            data.results.forEach(async (el) => {
+                const res = await fetch(el.url);
+                const data = await res.json();
 
-        setPokemons((pokemons) => [...pokemons, pokemon]);
-        });
-    }
-}, [isPending]);
+                let pokemon = {
+                    id: data.id,
+                    name: data.name,
+                    img: data.sprites.front_default,
+                };
 
-  return (
-    <>
-      {pokemons.length === 0 ? (
-        <img className="loader" src="loader.svg" alt="loader" />
-      ) : (
-        pokemons.map((el) => <Card key={el.id} url={el.img} name={el.name} />)
-      )}
-    </>
-  );
+                setPokemons((pokemons) => [...pokemons, pokemon]);
+            });
+        }
+    }, [isPending]);
+
+    if (error)
+        return (
+            <h2>
+                {error.statusText} | Error: {error.status}
+            </h2>
+        );
+    else
+        return (
+            <>
+                {pokemons.length === 0 ? (
+                    <img className="loader" src="loader.svg" alt="loader" />
+                ) : (
+                    pokemons.map((el) => (
+                        <Card key={el.id} url={el.img} name={el.name} />
+                    ))
+                )}
+            </>
+        );
 }
